0ea29588cee9fba34d6b38ea6e6e26012476c40d
conico <nico.kasprzyk@gmx.de>
2024-01-05 20:01:35 +0100

    * Added some helper functions. * Go back to running implementation with InvalidateRect.

--- Resource.h
+++ Resource.h
@@ -11,6 +11,7 @@
 #define IDI_VARDIAN                     107
 #define IDI_SMALL                       108
 #define IDC_VARDIAN                     109
+#define IDT_TIMER_COPY                  110
 #define IDR_MAINFRAME                   128
 #define IDC_MFCLINK1                    1000
 #define IDHOMEPAGE                      1000
@@ -23,9 +24,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32772
 #define _APS_NEXT_CONTROL_VALUE         1007
-#define _APS_NEXT_SYMED_VALUE           110
+#define _APS_NEXT_SYMED_VALUE           111
 #endif
 #endif
--- src/helpers.h
+++ src/helpers.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <Windows.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 char* getErrorCodeDescription(long errorCode);
+
+#define SHOW_USED_MESSAGES 1
+
+wchar_t* GetMessageText(unsigned int msg);
+
+#ifdef SHOW_USED_MESSAGES
+void ShowUsedMessages(void);
+#endif
 
 #ifdef __cplusplus
 }
